2198796e3ac6007fdbd8efce6ff2a48c1d7ed5f0
mdiserio <maurizio.diserio@gmail.com>
2022-03-25 16:23:10 +0100

    first navigation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { Route } from "react-router";
+import MainHeader from "./components/MainHeader";
+import Products from "./pages/Products";
+import Welcome from "./pages/Welcome";
 function App() {
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
+    <>
+      <MainHeader />
+      <Route path="/welcome">
+        <Welcome></Welcome>
+      </Route>
+      <Route path="/products">
+        <Products></Products>
+      </Route>
+    </>
   );
 }
 
