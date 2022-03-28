eb15bab0c20352e567cb160801191ee2da9aca4c
mdiserio <maurizio.diserio@gmail.com>
2022-03-28 14:22:48 +0200

    react-router project

--- src/App.js
+++ src/App.js
@@ -1,30 +1,8 @@
-import { Route, Switch } from "react-router";
-import { Redirect } from "react-router-dom";
-import MainHeader from "./components/MainHeader";
-import Products from "./pages/Products";
-import ProductsDetail from "./pages/ProductsDetail";
-import Welcome from "./pages/Welcome";
 function App() {
   return (
-    <>
-      <MainHeader />
-      <main>
-        <Switch>
-          <Route path="/" exact>
-            <Redirect to="/welcome" />
-          </Route>
-          <Route path="/welcome">
-            <Welcome></Welcome>
-          </Route>
-          <Route path="/products" exact>
-            <Products></Products>
-          </Route>
-          <Route path="/products/:productId">
-            <ProductsDetail></ProductsDetail>
-          </Route>
-        </Switch>
-      </main>
-    </>
+    <div>
+
+    </div>
   );
 }
 
