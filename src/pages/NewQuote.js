e91950d21c0e6b3e45f0e4eb5775396df0e76bf9
mdiserio <maurizio.diserio@gmail.com>
2022-03-28 17:14:32 +0200

    not found page

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,8 +1,12 @@
+import { useHistory } from "react-router-dom";
 import QuoteForm from "../components/quotes/QuoteForm";
 
 const NewQuote = () => {
+  const history = useHistory(); //to pass from a page to another page, object
   const onAddQuoteHandler = (newQuote) => {
     console.log(newQuote);
+
+    history.push("/quotes"); //push allow back button, replace not allow
   };
 
   return <QuoteForm onAddQuote={onAddQuoteHandler} />;
